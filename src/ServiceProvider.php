0e0986254137dac82ba3d08a7fe7de0128f04b22
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2025-08-16 09:22:02 -0700

    v74.0.0

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,7 +15,7 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider;
  */
 class ServiceProvider extends AuthServiceProvider
 {
-    public const VERSION = '73.0.0';
+    public const string VERSION = '74.0.0';
 
     protected string $package = 'playground-http';
 
